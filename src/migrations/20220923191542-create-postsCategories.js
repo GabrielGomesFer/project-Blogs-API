d9e6afc6aecb8701809b5bec16edf37e7163d17b
GabrielGomesFer <gabriel.ferreira2002@hotmail.com>
2022-09-23 17:04:53 -0300

    requisito 1

---
+++ src/migrations/20220923191542-create-postsCategories.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', { 
+      post_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        }
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'categories',
+          key: 'id',
+        }
+      },
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+
+  }
+};
