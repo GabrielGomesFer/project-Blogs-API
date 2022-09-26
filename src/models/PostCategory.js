d55b8a3849e789ca35befbb20bc7c183e3abeb68
GabrielGomesFer <gabriel.ferreira2002@hotmail.com>
2022-09-26 14:43:50 -0300

    post category corrigido

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,7 +1,17 @@
 const PostCategoryModel = (sequelize, DataTypes) => {
     const definePostCategory = sequelize.define('PostCategory', {
-        post_id: DataTypes.INTEGER,
-        category_id: DataTypes.INTEGER,
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            
+        },
     },
     {
         timestamps: false,
