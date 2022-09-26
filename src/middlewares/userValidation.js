c1f3ab776d0012e28c8c6ca2597aeb915e3646fb
GabrielGomesFer <gabriel.ferreira2002@hotmail.com>
2022-09-26 16:13:51 -0300

    erro de type

--- src/middlewares/userValidation.js
+++ src/middlewares/userValidation.js
@@ -1,7 +1,7 @@
 const userValidation = async (req, res, next) => {
     const { email, password } = req.body;
 
-    if (!!email || !password) {
+    if (!email || !password) {
         return res.status(400).json({ message: 'Some required fields are missing' });
     }
 
